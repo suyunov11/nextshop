6886307b1d12a19bac3934cfdd37a6f89aaae4d3
byoungcheon lee <lbcsultan@gmail.com>
2022-10-14 10:52:33 +0900

    9-CreateLoginForm

--- pages/cart.js
+++ pages/cart.js
@@ -96,7 +96,7 @@ function CartScreen() {
               <li>
                 <button
                   className="primary-button w-full"
-                  onClick={() => router.push('/shipping')}
+                  onClick={() => router.push('login?redirect=/shipping')}
                 >
                   Check out
                 </button>
