223cc300c546419596a09a316a06dd980a2434ad
byoungcheon lee <lbcsultan@gmail.com>
2022-10-14 09:54:56 +0900

    8-SaveItems

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,16 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useContext, useEffect, useState } from 'react'
+import { Store } from '../utils/Store'
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store)
+  const { cart } = state
+  const [cartItemsCount, setCartItemsCount] = useState(0)
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0))
+  }, [cart.cartItems])
+
   return (
     <div>
       <Head>
@@ -17,7 +26,14 @@ export default function Layout({ title, children }) {
             </Link>
             <div>
               <Link href="/cart">
-                <a className="p-2">Cart</a>
+                <a className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
               </Link>
               <Link href="/login">
                 <a className="p-2">Login</a>
